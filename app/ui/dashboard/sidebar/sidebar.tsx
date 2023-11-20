63c18a60b32a4bdc267550aefb6d79a6270bbbfc
Drakkarrr <junrey404@gmail.com>
2023-11-21 01:53:17 +0800

    menuitems mapped to sidebar

--- app/ui/dashboard/sidebar/sidebar.tsx
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import styles from './sidebar.module.css';
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+} from 'react-icons/md';
 
 const Sidebar = () => {
   const menuItems = [
@@ -14,23 +26,64 @@ const Sidebar = () => {
         {
           title: 'Users',
           path: '/dashboard/users',
-          icon: 'users',
+          icon: <MdSupervisedUserCircle />,
         },
         {
           title: 'Products',
           path: '/dashboard/products',
-          icon: 'products',
+          icon: <MdShoppingBag />,
         },
         {
           title: 'Transactions',
           path: '/dashboard/transactions',
-          icon: 'transactions',
+          icon: <MdAttachMoney />,
+        },
+      ],
+    },
+    {
+      title: 'Analytics',
+      list: [
+        {
+          title: 'Revenue',
+          path: '/dashboard/revenue',
+          icon: <MdWork />,
+        },
+        {
+          title: 'Reports',
+          path: '/dashboard/reports',
+          icon: <MdAnalytics />,
+        },
+        {
+          title: 'Teams',
+          path: '/dashboard/teams',
+          icon: <MdPeople />,
+        },
+      ],
+    },
+    {
+      title: 'User',
+      list: [
+        {
+          title: 'Settings',
+          path: '/dashboard/settings',
+          icon: <MdOutlineSettings />,
+        },
+        {
+          title: 'Help',
+          path: '/dashboard/help',
+          icon: <MdHelpCenter />,
         },
       ],
     },
   ];
 
-  return <div className={styles.container}>Sidebar</div>;
+  return (
+    <div className={styles.container}>
+      {menuItems.map((items) => (
+        <li key={items.title}>{items.title}</li>
+      ))}
+    </div>
+  );
 };
 
 export default Sidebar;
