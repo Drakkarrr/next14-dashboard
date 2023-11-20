bcc06b42d0678f9cbae4e6fbbd7d05d67b1010e6
Drakkarrr <junrey404@gmail.com>
2023-11-21 01:45:34 +0800

    added sidebar menu items

--- app/ui/dashboard/sidebar/sidebar.tsx
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import styles from './sidebar.module.css';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const menuItems = [
+    {
+      title: 'Pages',
+      list: [
+        {
+          title: 'Dashboard',
+          path: '/dashboard',
+          icon: <MdDashboard />,
+        },
+        {
+          title: 'Users',
+          path: '/dashboard/users',
+          icon: 'users',
+        },
+        {
+          title: 'Products',
+          path: '/dashboard/products',
+          icon: 'products',
+        },
+        {
+          title: 'Transactions',
+          path: '/dashboard/transactions',
+          icon: 'transactions',
+        },
+      ],
+    },
+  ];
+
+  return <div className={styles.container}>Sidebar</div>;
 };
 
 export default Sidebar;
