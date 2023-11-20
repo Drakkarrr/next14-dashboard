69606cf908684ed81e1a9b5bf58a4de3887b7d82
Drakkarrr <junrey404@gmail.com>
2023-11-21 01:33:20 +0800

    added module styles on

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,17 @@
 import React, { ReactNode } from 'react';
 import Sidebar from '@/app/ui/dashboard/sidebar/sidebar';
 import Navbar from '@/app/ui/dashboard/navbar/navbar';
+import styles from '@/app/ui/dashboard/dashboard.module.css';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <Sidebar />
-      <div>
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
         <Navbar />
-        <div>{children}</div>
+        {children}
       </div>
     </div>
   );
